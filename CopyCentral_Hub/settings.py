f067b0e8cd09c866ce44b82e812a387471cbd3bd
Tomasz Kulowski <tomek987125@gmail.com>
2024-03-05 18:33:48 +0100

    feature: Create employees app

--- CopyCentral_Hub/settings.py
+++ CopyCentral_Hub/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'authentication.apps.AuthenticationConfig',
+    'employees.apps.EmployeesConfig',
 ]
 
 MIDDLEWARE = [
---
+++ employees/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class DepartmentChoices(models.IntegerChoices):
+    BOSS = (0, 'Boss')
+    MANAGER = (1, 'Manager')
+    OFFICE = (2, 'Office staff')
+    TECHNICIAN = (3, 'Technician')
+    SELLER = (4, 'Seller')
+    WAREHOUSE = (5, 'Warehouse staff')
+
+
+class Employee(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='employee')
+    department = models.SmallIntegerField(choices=DepartmentChoices.choices)
+
+    def __str__(self):
+        return self.user.username
