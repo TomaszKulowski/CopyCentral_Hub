c62c1e69a15eeaf6a74edeb12324754926bd7fa9
Tomasz Kulowski <kulowski.tomasz@gmail.com>
2024-07-16 20:33:52 +0200

    feature: Create notifications app

--- employees/models.py
+++ employees/models.py
@@ -20,6 +20,7 @@ class Employee(models.Model):
         verbose_name=_('User'),
     )
     department = models.SmallIntegerField(_('Department'), choices=DepartmentChoices.choices)
+    phone_number = models.PositiveBigIntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
---
+++ notifications/admin.py
@@ -0,0 +1,50 @@
+from django.core.cache import cache
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponseRedirect
+from .tasks import notifications_task
+
+from .models import Notification, NotificationSettings
+from .utils import mark_orders_as_sent
+
+
+@admin.register(NotificationSettings)
+class NotificationSettingsAdmin(admin.ModelAdmin):
+    change_list_template = "notifications/templates/notifications/settings.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('send_notifications/', self.admin_site.admin_view(self.send_notifications), name='send_notifications'),
+        ]
+        return custom_urls + urls
+
+    def send_notifications(self, request):
+        if not cache.get('notification_enabled'):
+            mark_orders_as_sent()
+            notifications_task.delay()
+            cache.set('notification_enabled', True, timeout=None)
+            self.message_user(request, "Notification enabled.")
+        else:
+            self.message_user(request, "Notifications are already enabled.")
+
+        return HttpResponseRedirect("../")
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['send_notifications_url'] = 'send_notifications/'
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['receiver_name', 'phone_number', 'message', 'sent', 'created_at']
+    search_fields = ['receiver_name', 'phone_number']
+    list_per_page = 20
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
